98ce50f06fdb9db4f5f8dd117ecf39a905860f4c
hiYifang <a6426320@gmail.com>
2022-05-03 22:39:29 +0800

    fix: 修改 Schema

--- models/posts.js
+++ models/posts.js
@@ -2,49 +2,53 @@
 const mongoose = require('mongoose');
 
 // 建立 Schema
-const postsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, '貼文姓名未填寫']
-  },
-  tags: [
-    {
+const postsSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      required: [true, '貼文標籤 tags 未填寫']
-    }
-  ],
-  type: {
-    type: String,
-    enum:['group','person'],
-    required: [true, '貼文類型 type 未填寫']
-  },
-  image: {
-    type: String,
-    default: ""
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-    select: false
-  },
-  content: {
-    type: String,
-    required: [true, 'Content 未填寫'],
-  },
-  likes: {
-    type: Number,
-    default: 0
-  },
-  comments:{
-    type: Number,
-    default: 0
-  },
-});
+      required: [true, '貼文姓名 name 未填寫']
+    },
+    tags: [
+      {
+        type: String,
+        required: [true, '貼文標籤 tags 未填寫']
+      }
+    ],
+    type: {
+      type: String,
+      enum: ['group', 'person'],
+      required: [true, '貼文類型 type 未填寫']
+    },
+    image: {
+      type: String,
+      default: ""
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
+      select: false
+    },
+    content: {
+      type: String,
+      required: [true, '貼文內容 content 未填寫'],
+    },
+    likes: {
+      type: Number,
+      default: 0
+    },
+    comments: {
+      type: Number,
+      default: 0
+    },
+  }, {
+    versionKey: false,
+  }
+);
 
 // 建立 Model
-const posts = mongoose.model(
+const Posts = mongoose.model(
   'posts',
   postsSchema
 );
 
-module.exports = posts;
+module.exports = Posts;
